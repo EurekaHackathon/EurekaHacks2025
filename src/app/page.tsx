69180f39a3519a8d5108b249a29a9c8ed29a265f
Jake Comay <jake.comay@gmail.com>
2024-10-01 00:46:56 -0400

    add cool temporary landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
-  return (
-    <p>EurekaHacks 2025</p>
-  );
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="group hover:-translate-y-5 transition-all duration-700 hover:cursor-pointer">
+                <h1 className="text-[100px] font-bold m-0">EurekaHacks <span className="bg-primaryGradient bg-clip-text text-transparent">2025</span></h1>
+                <div className="w-0 group-hover:w-full h-[20px] bg-primaryGradient transition-all duration-700"></div>
+            </div>
+        </div>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            backgroundImage: {
+                primaryGradient: "linear-gradient(to right, #ff69eb, #ff86c8, #ffa3a5, #ffbf81, #ffdc5e)"
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
