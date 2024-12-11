d6f879d0fcdb2624c8441a02eae280a67d092877
Jake Comay <jake.comay@gmail.com>
2024-12-10 22:41:14 -0500

    add href for 2024 link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import * as motion from "framer-motion/client"
+"use client";
+
+import { motion, useAnimation } from "framer-motion";
 import { ArrowRight } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="absolute size-full overflow-hidden -z-20">
                 <img src="gradient.png" className="absolute blur-sm scale-[200%] left-0 top-0 translate-x-[-30%] translate-y-[-30%]" />
-                <img src="gradient.png" className="absolute blur-sm scale-[200%] right-0 bottom-0 translate-x-[30%] translate-y-[30%] rotate-180" />
+                <img src="gradient.png" className="absolute blur-sm sc
+                
+                const ale-[200%] right-0 bottom-0 translate-x-[30%] translate-y-[30%] rotate-180" />
             </div>
 
             <div className="flex flex-col items-center font-[Agrandir]">
@@ -42,33 +47,46 @@ export default function Home() {
 
             <div className="absolute bottom-20 flex flex-col items-center font-bold text-lg tracking-widest text-five m-4 gap-5">
                 <p>@Eureka_hacks</p>
-                <motion.a href="https://eurekahacks.ca" className="flex items-center gap-2" whileHover="hovered">
-                    {
-                        "2024>".split("").map((char, index) => {
-                            return (
-                                <motion.p
-                                    key={index}
-                                    variants={{
-                                        "initial": {
-                                            y: 0,
-                                        },
-                                        "hovered": {
-                                            y: [0, -5, 0],
-                                        }
-                                    }}
-                                    transition={{
-                                        delay: index * 0.1,
-                                        duration: 0.5,
-                                        ease: "easeInOut"
-                                    }}
-                                >
-                                    {char == ">" ? <ArrowRight/> : char}
-                                </motion.p>
-                            )
-                        })
-                    }
-                </motion.a>
+                <JiggleLink text="2024>" href="https://2024.eurekahacks.ca" />
             </div>
         </div>
+    );
+}
+
+function JiggleLink({
+    text, 
+    href
+}: {
+    text: string,
+    href: string
+}) {
+    const controls = useAnimation();
+
+    return (
+        <motion.a
+            href={href}
+            className="flex items-center gap-2 cursor-pointer" 
+            onHoverStart={() => controls.start({
+                y: [0, -5, 0],
+            })}
+        >
+            {
+                text.split("").map((char, index) => {
+                    return (
+                        <motion.p
+                            key={index}
+                            animate={controls}
+                            transition={{
+                                delay: index * 0.1,
+                                duration: 0.5,
+                                ease: "easeInOut"
+                            }}
+                        >
+                            {char == ">" ? <ArrowRight /> : char}
+                        </motion.p>
+                    )
+                })
+            }
+        </motion.a>
     );
 }
