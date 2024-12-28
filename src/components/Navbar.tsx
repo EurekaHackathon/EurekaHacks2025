2ba2cbaef1c8db6e5cf486eba9273dddb567282b
Aaron <aaronye1001@gmail.com>
2024-12-28 15:10:06 -0500

    Fix ios chrome bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,8 +10,12 @@ export default function Navbar() {
     const [isUserScrolled, setIsUserScrolled] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [clickedOnce, setClickedOnce] = useState(false);
+    // Fix for iOS Chrome
+    const [isOnIosChrome, setIsOnIosChrome] = useState(false);
 
     useEffect(() => {
+        setIsOnIosChrome(navigator.userAgent.includes("CriOS"));
+
         const handleScroll = () => {
             setIsUserScrolled(true);
             if (window.scrollY > 0) {
@@ -37,7 +41,7 @@ export default function Navbar() {
     return (
         <nav
             className={`fixed flex flex-col lg:items-center w-full top-0 z-40 text-gray-200 ease-in-out
-             ${isScrollPositionNotZero && !isMenuOpen ? "backdrop-brightness-95 backdrop-blur-xl" : ""}
+             ${(isScrollPositionNotZero || isOnIosChrome) && !isMenuOpen ? "backdrop-brightness-95 backdrop-blur-xl" : ""}
              ${isUserScrolled || clickedOnce ? "duration-500 lg:duration-200" : ""}
              ${isMenuOpen ? "backdrop-brightness-75 backdrop-blur-3xl" : ""}`}>
             <div className="hidden lg:flex items-center justify-between py-4 lg:w-[85%] 2xl:w-[60%]">
@@ -68,13 +72,15 @@ export default function Navbar() {
                 <Image className="w-10 md:w-12 h-auto" src={logo} alt="EurekaHACKS Logo"/>
                 <button onClick={toggleMenu}>
                     {isMenuOpen && <Icon icon="mdi:close" className="text-3xl md:text-4xl fade-in"/>}
-                    {!isMenuOpen && <Icon icon="mdi:menu" className={`text-3xl md:text-4xl ${clickedOnce ? "fade-in" : ""}`}/>}
+                    {!isMenuOpen &&
+                        <Icon icon="mdi:menu" className={`text-3xl md:text-4xl ${clickedOnce ? "fade-in" : ""}`}/>}
                 </button>
             </div>
             <div
                 className={`lg:hidden overflow-hidden transition-all duration-300 ease-in-out ${isMenuOpen ? "max-h-screen" : "max-h-0"}`}>
                 <div className="h-[100vh] fade-in flex flex-col items-center justify-center gap-8 md:gap-12">
-                    <div className="flex flex-col gap-4 md:gap-8 landscape:gap-4 text-xl md:text-4xl landscape:text-2xl font-semibold">
+                    <div
+                        className="flex flex-col gap-4 md:gap-8 landscape:gap-4 text-xl md:text-4xl landscape:text-2xl font-semibold">
                         <a href="#about" onClick={toggleMenu}>About</a>
                         <a href="#past" onClick={toggleMenu}>Past Years</a>
                         <a href="#sponsors" onClick={toggleMenu}>Sponsors</a>
